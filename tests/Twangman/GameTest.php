7585aa34fdf17b069bd5c94f641be4acaf3625c9
HughbertD <hugh.downer@gmail.com>
2015-04-28 21:29:46 +0100

    Mock out Twitter class, create tests for starting a game instance

--- tests/Twangman/GameTest.php
+++ tests/Twangman/GameTest.php
@@ -1,7 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hughdowner
- * Date: 28/04/2015
- * Time: 21:20
- */
+
+
+use App\Twangman\Game;
+
+class GameTest extends TestCase
+{
+
+    /**
+     * Creates the application.
+     *
+     * @return \Laravel\Lumen\Application
+     */
+    public function createApplication()
+    {
+        return require __DIR__ . '/../../bootstrap/app.php';
+    }
+
+    /**
+     * Test the game start up method
+     */
+    public function testGameStart()
+    {
+
+        $twitterTopic = 'TestTwitterTopic';
+        $trends = [
+            (object) ['trends' => [
+                (object) ['name' => $twitterTopic]
+            ]]
+        ];
+        $twitterOAuth = $this->getMockBuilder('Abraham\TwitterOAuth\TwitterOAuth')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $twitterOAuth->method('get')->willReturn($trends);
+        $game = new Game($twitterOAuth);
+        $game->start();
+
+        $this->assertEquals($game->word, $twitterTopic);
+        $this->assertEquals($game->numberTiles, strlen($twitterTopic));
+    }
+
+}
