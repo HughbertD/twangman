80cba1b225d2f184514a126b4944ce1020248737
HughbertD <hugh.downer@gmail.com>
2015-04-28 21:31:07 +0100

    Added placeholder view for home

---
+++ resources/views/home.php
@@ -0,0 +1,30 @@
+<DOCTYPE html>
+    <html lang="en">
+        <head>
+            <title>Twangman</title>
+
+            <!-- Bootstrap-->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        </head>
+        <body>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>Twangman - Twitter Hangman</h1>
+                        <p>Log in via Twitter to start a game</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+
+                    </div>
+                </div>
+            </div>
+        </body>
+    </html>
