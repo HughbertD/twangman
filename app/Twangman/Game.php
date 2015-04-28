48b43b864350e49bb809bb732da64c41423a52d9
HughbertD <hugh.downer@gmail.com>
2015-04-28 21:26:14 +0100

    Added .env.example file with required TwitterToken parameters

---
+++ app/Twangman/Game.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hughdowner
+ * Date: 28/04/2015
+ * Time: 19:23
+ */
+
+namespace Twangman;
+
+
+class Game {
+
+}
---
+++ tests/Twangman/GameTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hughdowner
+ * Date: 28/04/2015
+ * Time: 21:20
+ */
