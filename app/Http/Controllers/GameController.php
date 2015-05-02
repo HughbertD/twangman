3c2a817efbdbdf71c40c70b6166fcac298f36805
HughbertD <hugh.downer@gmail.com>
2015-05-02 11:26:40 +0100

    Deleted Home view, added tests and functionality for /guess/ routing

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -1,8 +1,9 @@
 <?php
 namespace App\Http\Controllers;
 
-use Abraham\TwitterOAuth\TwitterOAuth;
 use App\Twangman\Game;
+use Illuminate\Http\Response;
+use Exception;
 
 class GameController extends Controller
 {
@@ -12,10 +13,49 @@ class GameController extends Controller
         $this->game = $game;
     }
 
+    /**
+     * Start a new instance of a game
+     */
     public function start()
     {
-        //Start a new game
-        $this->game->start();
+        try{
+            $this->game->start();
+        } catch(Exception $e) {
+            die($e->getMessage());
+        }
+        return view('game', ['numberTiles' => $this->game->numberTiles]);
+    }
+
+    /**
+     * Take a guess with a letter
+     * @param $letter
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function guess($letter)
+    {
+        try{
+            $loaded = $this->game->load(session('game'));
+        } catch(Exception $e) {
+            return Response::create([], 404);
+        }
+
+        $matches = $this->game->guess($letter);
+        $success = !empty($matches);
+        $state = $this->game->state($success);
+
+        $response = [
+            'success' => !empty($matches),
+            'letter' => strtoupper($letter),
+            'matchPositions' => $matches,
+            'state' => $state
+        ];
+
+        $this->game->save();
+        if($state == Game::DEAD_STATE || $state == Game::WINNER_STATE) {
+            $this->game->end();
+        }
+
+        return Response::create($response, 200);
     }
 
 }
--- app/Twangman/Game.php
+++ app/Twangman/Game.php
@@ -3,6 +3,7 @@
 namespace App\Twangman;
 
 use Abraham\TwitterOAuth\TwitterOAuth;
+use Exception;
 
 /**
  * Class Game
@@ -48,6 +49,32 @@ class Game
     public $incorrectGuesses = [];
 
     /**
+     * Total lives (guesses) before you are hung!
+     */
+    const LIVES = 6;
+
+    /**
+     * Player is dead
+     */
+    const DEAD_STATE = "dead";
+
+    /**
+     * Player won
+     */
+    const WINNER_STATE = "winner";
+
+    /**
+     * Player next turn, found match
+     */
+    const MATCH_FOUND_STATE = "match";
+
+    /**
+     * Player next turn, no found match
+     */
+    const NO_MATCH_FOUND_STATE = "no-match";
+
+    /**
+     *
      * @param TwitterOAuth $oAuth
      */
     public function __construct(TwitterOAuth $oAuth)
@@ -62,16 +89,138 @@ class Game
     {
         //Get the current trending topics (top10)
         $availableTrends = $this->oAuth->get('trends/place', ['id' => 23424975]);
+        if(!is_array($availableTrends)) {
+            throw new Exception("Couldn't retrieve Twitter topics");
+        }
 
-        //Pick a trending topic at random
+        //Pick a trending topic at random, set as our gameWord
         $gameWord = $this->pickWord($availableTrends[0]->trends);
-
-        $this->word = $gameWord;
+        $this->setWord($gameWord);
 
         $this->numberTiles = $this->setNumberTiles($gameWord);
 
         //Store the game data to the session
+        $this->save();
+    }
+
+    /**
+     * Save the current game back to the session
+     *
+     */
+    public function save()
+    {
         session(['game' => $this]);
+    }
+
+    /**
+     * Load the current game from the session
+     * @param $game
+     * @return bool
+     * @throws Exception
+     */
+    public function load($game)
+    {
+        if(is_null($game)){
+            throw new Exception("Cannot load game");
+        }
+
+        $this->setWord($game->word);
+        $this->setNumberTiles($game->word);
+        $this->correctGuesses = $game->correctGuesses;
+        $this->incorrectGuesses = $game->incorrectGuesses;
+        $this->numberGuessesTaken = $game->numberGuessesTaken;
+        return true;
+    }
+
+    /**
+     * Finish a game
+     */
+    public function end()
+    {
+        session(['game' => NULL]);
+    }
+
+    public function guess($guess)
+    {
+        $guess = strtoupper($guess);
+        $wordArray = str_split($this->word);
+        $matches = [];
+        foreach($wordArray as $pos => $letter) {
+            if($letter == $guess) $matches[] = $pos;
+        }
+
+        $this->assignGuess($guess, !empty($matches));
+        return $matches;
+    }
+
+    /**
+     * Route the guess to the correct setter
+     * @param $guess
+     * @param $correct
+     */
+    public function assignGuess($guess, $correct)
+    {
+        ($correct === true) ? $this->addCorrectGuess($guess) : $this->addIncorrectGuess($guess);
+    }
+
+    /**
+     * Add a guess to the correctGuesses var
+     * @param $guess
+     */
+    public function addCorrectGuess($guess)
+    {
+        $this->correctGuesses[] = $guess;
+    }
+
+    /**
+     * Add a guess to the incorrectGuesses var, dock a life
+     * @param $guess
+     * @return Array
+     */
+    public function addIncorrectGuess($guess)
+    {
+        array_push($this->incorrectGuesses, $guess);
+        return $this->incorrectGuesses;
+    }
+
+    /**
+     * Gets the state of the player after their guess
+     * @return string
+     */
+    public function state($success)
+    {
+        if($this->isDead()) {
+            return self::DEAD_STATE;
+        }
+
+        if($this->isWinner()) {
+            return self::WINNER_STATE;
+        }
+
+        if($success) {
+            return self::MATCH_FOUND_STATE;
+        }
+
+        return self::NO_MATCH_FOUND_STATE;
+    }
+
+    /**
+     * Check if the player has run out of guesses
+     * @return bool
+     */
+    public function isDead()
+    {
+        return count($this->incorrectGuesses) >= self::LIVES;
+    }
+
+    /**
+     * Check if the player has won
+     * @return bool
+     */
+    public function isWinner()
+    {
+        $leftToGuess = array_diff(str_split($this->word), $this->correctGuesses);
+        return empty($leftToGuess);
     }
 
     /**
@@ -82,6 +231,15 @@ class Game
     {
         shuffle($availableTrends);
         return current($availableTrends)->name;
+    }
+
+    /**
+     * Setter for word
+     * @param $word
+     */
+    private function setWord($word)
+    {
+        $this->word = strtoupper($word);
     }
 
     /**
